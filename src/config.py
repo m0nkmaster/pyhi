6a107ee3f676ab24dd3a96308ef5db51f38db51d
Rob MacDonald <rob@robmacdonald.com>
2024-12-10 17:54:45 +0000

    Minor config change

--- src/config.py
+++ src/config.py
@@ -28,7 +28,7 @@ def get_sound_path(filename: str) -> str:
 @dataclass
 class AudioRecorderConfig:
     wake_word_silence_threshold: float = 0.5
-    response_silence_threshold: float = 3.0  # Increased from default
+    response_silence_threshold: float = 1.0  # Increased from default
     buffer_duration: float = 1.0
 
 
